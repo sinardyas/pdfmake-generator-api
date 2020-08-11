5bb3c99e549ad2eddf11d537f7ec42e1ea9a50cc
Sinar Dyas <sinardyas96@gmail.com>
2020-08-11 15:39:23 +0700

    add middleware

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/ping', (req, res) => res.json({ message: 'Im awake!' }));
+app.get('/wake-me-up', (req, res) => res.json({ message: 'Im awake!' }));
+
+app.use((req, res, next) => {
+  // -----------------------------------------------------------------------
+  // authentication middleware
+
+  const auth = { login: 'admin', password: 'supersecretpassw000rd' }; // change this
+
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next();
+  }
+
+  // Access denied...
+  res.set('WWW-Authenticate', 'Basic realm="401"'); // change this
+  return res.status(401).json({ status: 401, message: 'Authentication required.' }); // custom message
+});
+
 
 app.post('/v1/generate-pdf/pinjaman-online', (req, res) => {
   const docDefinition = pdfContent;
